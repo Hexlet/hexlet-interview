a324a22adeab577e2d65fdc40ae124f05a6cee58
Eugene Sinitsyn <djorka91@mail.ru>
2019-09-09 20:40:59 +0300

    Add Procfile

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname,  '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
   await app.listen(3000);
