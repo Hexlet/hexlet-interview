b95a43e41cb912ebc719ed1e936778baee6e5374
Mitry1974 <tomitry@gmail.com>
2019-09-12 00:48:47 +0300

    i18n translator init moved to bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { bootstrapApp } from './bootstrap';
-import * as i18n from 'i18n';
-
-i18n.configure({
-  locales: ['en', 'ru'],
-  cookie: 'interviewcookie',
-  directory: __dirname + '/locales'
-});
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(i18n.init);
   await bootstrapApp(app);
   await app.listen(process.env.PORT || 3000);
 }
