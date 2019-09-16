c2068ac3100e7dd76aa98cd9a2d7f15851b75bf8
Mitry1974 <tomitry@gmail.com>
2019-09-17 01:53:11 +0300

    user migration updated

--- src/db/migrations/1568273019623-user.ts
+++ src/db/migrations/1568273019623-user.ts
@@ -1,50 +1,39 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
-import { stringLiteral } from "@babel/types";
 
 export class user1568273019623 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.createTable(new Table({
-            name: 'user',
-            columns: [
-              {
-                name: 'id',
-                type: 'number',
-                length: '255',
-                isPrimary: true,
-                isGenerated: true,
-                generationStrategy: 'increment',              },
-              {
-                name: 'firstname',
-                type: 'varchar',
-                length: '255',
-              },
-              {
-                name: 'lastname',
-                type: 'varchar',
-                length: '255',
-              },
-              {
-                name: 'email',
-                type: 'varchar',
-                length: '255',
-              },
-              {
-                name: 'password',
-                type: 'varchar',
-                length: '255',
-              },
-              {
-                name: 'enabled',
-                type: 'boolean',
-              },
-              {
-                name: 'role',
-                type: 'varchar',
-                length: '255',
-              }
-          ],
-          }), true);
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',        },
+          {
+            name: 'firstname',
+            type: 'varchar',
+          },
+          {
+            name: 'lastname',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'enabled',
+            type: 'integer',
+          },
+      ],
+      }), true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
