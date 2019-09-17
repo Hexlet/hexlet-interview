980cdbd20477124f762c993fdecca44152c2ead1
Mitry1974 <tomitry@gmail.com>
2019-09-17 10:39:03 +0300

    user create dto validation added

--- src/modules/user/dto/user.create.dto.ts
+++ src/modules/user/dto/user.create.dto.ts
@@ -1,6 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class UserCreateDto {
+  @IsNotEmpty()
   readonly firstName: string;
+
   readonly lastName: string;
+
+  @IsNotEmpty()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
