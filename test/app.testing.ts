0aeb748039c57f9ffff4ced57bb0b574abd40208
Eugene Sinitsyn <djorka91@mail.ru>
2019-09-18 22:32:24 +0300

    Fix tests

--- test/app.testing.ts
+++ test/app.testing.ts
@@ -1,13 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './../src/modules/app/app.module';
 import { bootstrap } from './bootstrap';
-import { getRepository, Repository, BaseEntity } from 'typeorm';
-import { Request } from '../src/modules/request/request.entity';
-import { Interview } from '../src/modules/interview/interview.entity';
 import { INestApplication } from '@nestjs/common';
-import { User } from '../src/modules/user/user.entity';
 
-export const createTestingApp = async (): Promise<TestingApp> => {
+export const createTestingApp = async (): Promise<INestApplication> => {
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
@@ -17,21 +13,5 @@ export const createTestingApp = async (): Promise<TestingApp> => {
 
   await app.init();
 
-  return { ...app, ...{
-    http: app.getHttpServer(),
-    close: app.close,
-    repos: {
-      request: getRepository(Request),
-      interview: getRepository(Interview),
-      user: getRepository(User),
-    },
-  }};
+  return app;
 };
-
-export type TestingApp = INestApplication & TestingHelpers;
-
-interface TestingHelpers {
-  http: any;
-  close: () => Promise<void>;
-  repos: { [key: string]: Repository<BaseEntity> };
-}
