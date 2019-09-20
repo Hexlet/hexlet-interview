158a43b1d72526491cdad33dddb51f3d85233f2d
Eugene Sinitsyn <djorka91@mail.ru>
2019-09-20 20:15:09 +0300

    Add past interview model
    - add table
    - add entity
    - change show on main page real past interviews

--- src/db/migrations/1568998724054-past_interview.ts
+++ src/db/migrations/1568998724054-past_interview.ts
@@ -1,12 +1,13 @@
-import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 import { datetime } from '../datetime.adapter';
 
-export class interview1568057021616 implements MigrationInterface {
+export class pastInterview1568998724054 implements MigrationInterface {
+
   public async up(queryRunner: QueryRunner): Promise<any> {
     const databaseType = queryRunner.connection.options.type;
     await queryRunner.createTable(
       new Table({
-        name: 'interview',
+        name: 'past_interview',
         columns: [
           {
             name: 'id',
@@ -16,17 +17,13 @@ export class interview1568057021616 implements MigrationInterface {
             generationStrategy: 'increment',
           },
           {
-            name: 'state',
-            type: 'varchar',
-          },
-          {
             name: 'interviewer',
             type: 'varchar',
             isNullable: true,
           },
           {
-            name: 'interviewee_id',
-            type: 'integer',
+            name: 'interviewee',
+            type: 'varchar',
           },
           {
             name: 'profession',
@@ -35,11 +32,6 @@ export class interview1568057021616 implements MigrationInterface {
           {
             name: 'position',
             type: 'varchar',
-          },
-          {
-            name: 'description',
-            type: 'text',
-            isNullable: true,
           },
           {
             name: 'video_link',
@@ -55,24 +47,14 @@ export class interview1568057021616 implements MigrationInterface {
             name: 'created_at',
             type: datetime(databaseType),
           },
-          {
-            name: 'updated_at',
-            type: datetime(databaseType),
-          },
         ],
       }),
       true,
     );
-
-    await queryRunner.createForeignKey('interview', new TableForeignKey({
-      columnNames: ['interviewee_id'],
-      referencedColumnNames: ['id'],
-      referencedTableName: 'user',
-      onDelete: 'CASCADE',
-    }));
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.dropTable('interview');
+    await queryRunner.dropTable('past_interview');
   }
+
 }
--- src/modules/interview/interview.service.ts
+++ src/modules/interview/interview.service.ts
@@ -1,12 +1,16 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Interview } from './interview.entity';
+import { PastInterview } from './past-interview.entity';
 import { Repository } from 'typeorm';
 import { InterviewCreateDto } from './dto/interview.create.dto';
 import { User } from '../user/user.entity';
+import {Injectable} from '@nestjs/common';
 
+@Injectable()
 export class InterviewService {
   constructor(
     @InjectRepository(Interview) private readonly repo: Repository<Interview>,
+    @InjectRepository(PastInterview) private readonly pastInterviewRepo: Repository<PastInterview>,
   ) {}
 
   async findAll() {
@@ -22,5 +26,10 @@ export class InterviewService {
     const interview = this.repo.create(entity);
 
     return await interview.save();
+  }
+
+  async getPast() {
+    // merge real passed interview with interviews which was in past before this werb-service
+    return this.pastInterviewRepo.find();
   }
 }
