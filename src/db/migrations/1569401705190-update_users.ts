b47c853cd931e1f96931052b7d3b9ce063cfcb25
grigori-gru <grgr14@mail.ru>
2019-09-27 15:55:37 +0300

    change verify token name

--- src/db/migrations/1569401705190-update_users.ts
+++ src/db/migrations/1569401705190-update_users.ts
@@ -4,7 +4,7 @@ export class updateUsers1569401705190 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.addColumns('user', [
       new TableColumn({
-        name: 'token',
+        name: 'confirmation_token',
         type: 'uuid',
         isNullable: true,
       }),
@@ -17,7 +17,7 @@ export class updateUsers1569401705190 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.dropColumn('user', 'token');
+    await queryRunner.dropColumn('user', 'confirmation_token');
     await queryRunner.dropColumn('user', 'verified');
   }
 }
