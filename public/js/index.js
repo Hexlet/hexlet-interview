0d5a9a85033686f6ce1ba0f15d75503268c20020
mput <putintsev@gmail.com>
2019-10-23 23:14:20 +0300

    Change layout to serve local assets.

--- public/js/index.js
+++ public/js/index.js
@@ -2,5 +2,5 @@ import 'bootstrap';
 import 'jquery-ujs';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/regular';
-// import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/solid';
 // import '@fortawesome/fontawesome-free/js/brands';
