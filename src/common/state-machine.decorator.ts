9b028e1b1d8e51c3f018451dd4c2f0bf3e612a25
Eugene Sinitsyn <djorka91@mail.ru>
2019-10-28 10:56:00 +0300

    WIP

--- src/common/state-machine.decorator.ts
+++ src/common/state-machine.decorator.ts
@@ -1,12 +1,48 @@
-import * as JsStateMachine from 'javascript-state-machine';
+import * as JSM from 'javascript-state-machine';
+
+declare global {
+  interface Function {
+    clone(): any;
+  }
+}
+
+Function.prototype.clone = function() {
+  var that = this;
+  var temp = function temporary() { return that.apply(this, arguments); };
+  for(var key in this) {
+    if (this.hasOwnProperty(key)) {
+      temp[key] = this[key];
+    }
+  }
+  return temp;
+};
 
 export function StateMachine(definition: any) {
   return function<T extends { new (...args: any[]): {} }>(target: T): T {
     return class extends target {
       constructor(...args: any[]) {
         super(args);
-        const e = JsStateMachine.factory(this.constructor, definition);
-        e.prototype._fsm();
+        const cstorcopy = this.constructor.clone();
+        let e = JSM.factory(cstorcopy, definition);
+        for (let [key, value] of Object.entries(e.prototype)) {
+          if (key !== 'state') {
+            this.constructor.prototype[key] = value;
+          }
+        }
+        //e = undefined;
+        Object.defineProperties(this.constructor.prototype, {
+          state: {
+            configurable: true,
+            enumerable:   true,
+            get: function() {
+              return this._fsm.state;
+            },
+            set: function(state) {
+              this._fsm.state = state;
+            }
+          }
+        });
+        this.constructor.prototype._fsm();
       }
     };
   };
---
+++ test-fsm.ts
@@ -0,0 +1,6 @@
+import { Interview } from './src/modules/interview/interview.entity';
+
+const interview = new Interview();
+console.log(interview);
+console.log(interview.state)
+
